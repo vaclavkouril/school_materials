dad7d33d6778bc490083bacfc49902813429d832
Vaclav <vaclav.kouril02@gmail.com>
2025-05-28 20:25:36 +0200

    added formating

---
+++ docs/js/math-render.js
@@ -0,0 +1,8 @@
+document$.subscribe(() => {
+  renderMathInElement(document.body, {
+    delimiters: [
+      {left: "$$", right: "$$", display: true},
+      {left: "$", right: "$", display: false}
+    ]
+  });
+});
