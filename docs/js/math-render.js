0e83df673f485771742590565bd6d059196ad1c0
Vaclav <vaclav.kouril02@gmail.com>
2025-05-28 20:35:22 +0200

    added more styling

--- docs/js/math-render.js
+++ docs/js/math-render.js
@@ -2,7 +2,10 @@ document$.subscribe(() => {
   renderMathInElement(document.body, {
     delimiters: [
       {left: "$$", right: "$$", display: true},
-      {left: "$", right: "$", display: false}
-    ]
+      {left: "$", right: "$", display: false},
+      {left: "\\(", right: "\\)", display: false},
+      {left: "\\[", right: "\\]", display: true}
+    ],
+    throwOnError: false
   });
 });
